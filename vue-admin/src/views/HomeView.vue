c39ca4661c92ca7512c1f995ef6a6f9e887eee2a
Yang <lzlyvip@gmail.com>
2022-11-23 10:49:01 +1100

    update

---
+++ vue-admin/src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <el-button type="primary">主要按钮</el-button>
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
