256e1e35f16ed949bedb98fcc7899307d463b11f
Yang <lzlyvip@gmail.com>
2022-11-23 13:39:51 +1100

    update

---
+++ springboot/src/main/java/com/example/springboot/entity/User.java
@@ -0,0 +1,12 @@
+package com.example.springboot.entity;
+
+public class User {
+    private int id;
+    private String name;
+    private String username;
+    private int age;
+    private String sex;
+    private String phone;
+    private String address;
+
+}
