ba73aae73b7c433f03bca83562af814e5b9d2b98
Yang <lzlyvip@gmail.com>
2022-11-30 14:17:36 +1100

    update

--- vue-admin/vue.config.js
+++ vue-admin/vue.config.js
@@ -1,22 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  devServer: {
-    proxy: {
-        '/V1': {
-            target: 'http://localhost:9090',
-            changeOrigin: true,
-            pathRewrite: {
-                '^/V1': ''
-            }
-        },
-        '/V2': {
-            target: 'https://source.unsplash.com',
-            changeOrigin: true,
-            pathRewrite: {
-                '^/V2': ''
-            }
-        },
-        
-    }
 })
